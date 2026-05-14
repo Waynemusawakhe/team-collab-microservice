3722d9f032cb7e94ed23198a2e22b32892df01ac
Musawakhe Chipeya <134807443+Waynemusawakhe@users.noreply.github.com>
2026-05-14 10:32:32 +0200

    Winston

--- auth-service/app.js
+++ auth-service/app.js
@@ -1,4 +1,4 @@
-const logger = require("../utils/logger");
+const logger = require("./src/utils/logger");
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
